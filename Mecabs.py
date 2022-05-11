0b9331e5f2f2746d714b611c4b0da9056339209c
yoonan <alswn81616@gmail.com>
2022-05-11 20:45:00 +0900

    형태소 분석

--- Mecabs.py
+++ Mecabs.py
@@ -1,8 +1,12 @@
 from konlpy.tag import Mecab
 
 mecab = Mecab()
-data = mecab.pos("민주야 뭐해? 내일 영화보러갈래?")
+data = mecab.pos("민주야 뭐해? 내일 영화 만들러 갈래?")
 
 for dataList in data:
     print(dataList)
+print("")
+for dataList in data:
+    if( 'MAG' in dataList ): # 시간추출
+        print(dataList)
 
