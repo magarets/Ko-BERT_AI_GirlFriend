2a66ec6925cc1c8ef10ff80c44fbc34edb5d1b85
yoonan <101767824+magarets@users.noreply.github.com>
2022-05-26 02:09:45 +0900

    Update PingPongAPI.py

--- PingPongAPI.py
+++ PingPongAPI.py
@@ -6,12 +6,12 @@ from telegram.ext import Updater, MessageHandler, Filters
 from PingPongTool import PingPong  # 핑퐁툴 모듈 임포트
 
 # telegram api_key, bot, chat_id setting
-api_key = '5333196879:AAHyqMVrtXCS3DDQhnjVpgVrDf_GX5NxJ3Q' # minju's api_key
+api_key = '' # minju's api_key
 bot = telegram.Bot(token=api_key)
 chat_id = 5385808815
 
 # telegram updater
-updater = Updater(token='5333196879:AAHyqMVrtXCS3DDQhnjVpgVrDf_GX5NxJ3Q')
+updater = Updater(token='')
 dispatcher = updater.dispatcher
 updater.start_polling()
 
@@ -21,8 +21,8 @@ userList = []
 ''' pingpong start '''
 
 # URL, Token 생성
-URL = "https://builder.pingpong.us/api/builder/627c7214e4b0d7787e92b6fe/integration/v0.2/custom/{sessionId}"  # 핑퐁 커스텀 API 사이트에서 링크
-Authorization = "Basic a2V5OjQ0ZDdjN2ZmYTJkYTFmNTA2Zjg4ODhlMjEyYzk3MDg3"  # 핑퐁 커스텀 API 사이트에서 인증 토큰
+URL = ""  # 핑퐁 커스텀 API 사이트에서 링크
+Authorization = ""  # 핑퐁 커스텀 API 사이트에서 인증 토큰
 
 Ping = PingPong(URL, Authorization)  # 핑퐁 클래스 선언
 sessionID = 'a'
